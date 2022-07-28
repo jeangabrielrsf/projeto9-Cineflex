c2b869bd29d26843ff8afc85bb8f7ff26984ef2a
Jean Frusca <jeang96@gmail.com>
2022-07-28 16:20:04 -0300

    finalizado layout da página de seleção de sessões. Footer feito e já com poster e título do filme escolhido

--- src/SessionSelect/SessionSelect.js
+++ src/SessionSelect/SessionSelect.js
@@ -7,45 +7,58 @@ export default function SessionSelect () {
 
     const { idFilme } = useParams();
     const [sessions, setSessions] = useState([]);
+    const [movieTitle, setMovieTitle] = useState("");
+    const [moviePoster, setMoviePoster] = useState("");
 
     useEffect(() => {
         const promise = axios.get(`https://mock-api.driven.com.br/api/v5/cineflex/movies/${idFilme}/showtimes`);
         
         promise.then(result => {
             setSessions(result.data.days);
+            setMoviePoster(result.data.posterURL);
+            setMovieTitle(result.data.title);
         });
     }, []);
     return (
         <>
-            <div className="select-session">
-                <h2>Selecione o horário</h2>
+            <div className="container-select">
+                <div className="select-session">
+                    <h2>Selecione o horário</h2>
+                </div>
+
+                {sessions.map((session,index) => {
+
+                    return (
+                        <div className="sessions" key={index}>
+                            <div className="session-day">
+                                {session.weekday} - {session.date}
+                            </div>
+                            <div className="session-hours">
+                                {session.showtimes.map((show, index) => {
+                                    return (
+                                        <div className="hour" key={index}>
+                                            {show.name}
+                                        </div>
+                                    );
+                                })}
+                            </div>
+
+                        </div>
+                    );
+                })}
+
+                
             </div>
-
-            {sessions.map((session,index) => {
-
-                return (
-                    <div className="sessions" key={index}>
-                        <div className="session-day">
-                            {session.weekday} - {session.date}
-                        </div>
-                        <div className="session-hours">
-                            {session.showtimes.map((show, index) => {
-                                return (
-                                    <div className="hour" key={index}>
-                                        {show.name}
-                                    </div>
-                                );
-                            })}
-                        </div>
-
-                    </div>
-                );
-            })}
-
             <footer>
-
+                <div className="movie-poster">
+                    <img src={moviePoster} alt="" />
+                </div>
+                <div className="movie-title">
+                    {movieTitle}
+                </div>
             </footer>
         </>
+        
 
     )
 }
