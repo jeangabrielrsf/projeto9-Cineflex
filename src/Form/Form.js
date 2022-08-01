12ad46dc3ee8e648a01dde5abb07dca6260b3bdb
Jean Frusca <jeang96@gmail.com>
2022-08-01 17:02:24 -0300

    correção no formato de exibição do cpf na tela de sucesso

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -26,16 +26,32 @@ export default function Form ({
             cpf,
         });
 
+        const newCpf = formatCpf();
+        
         request.then (result => {
             setClientInfo({
                 name,
-                cpf,
+                cpf:newCpf,
             })
             setName("");
             setCpf("");
             navigate("/sucesso");
         })
 
+    }
+
+    function formatCpf () {
+        const firstThree = cpf.slice(0,3);
+        const secondThree = cpf.slice(3,6);
+        const thirdThree = cpf.slice(6,9);
+        const lastTwo = cpf.slice(9);
+        console.log(firstThree);
+        console.log(secondThree);
+        console.log(thirdThree);
+        console.log(lastTwo);
+        return (firstThree + "." + secondThree + "."
+                        + thirdThree + "-" + lastTwo);
+         
     }
 
 
@@ -47,7 +63,7 @@ export default function Form ({
             <input type="text" id="userName" required placeholder="Digite seu nome..." onChange={e => setName(e.target.value)} value={name}/>
 
             <label htmlFor="userCPF">CPF do comprador:</label>
-            <input type="text" id="userCPF" required placeholder="Digite seu CPF(apenas números)... " maxLength={11} pattern="[0-9]{11}" value={cpf} onChange={e => setCpf(e.target.value)}/>
+            <input type="text" id="userCPF" required placeholder="Digite seu CPF(apenas números)... " maxLength={11} pattern="[0-9]{11}" title="Apenas números!" value={cpf} onChange={e => setCpf(e.target.value)}/>
 
             <button type="submit">Reservar assento(s)</button>
         </form>
